d0b944e21797c337f5c2a4751610bf64d8ab20d7
reraiden <resibarani@gmail.com>
2020-12-11 00:42:48 +0700

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    var user = Knack.getUserAttributes();
-          if (user.email == null){
-            window.location.replace("https://res-spectacular-project-95211e.webflow.io/#login/");
-  }
-  });
