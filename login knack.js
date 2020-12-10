51f79921c2cd9e1dfe9dcd1cb59afc314f85b0d6
reraiden <resibarani@gmail.com>
2020-12-11 00:40:17 +0700

    Update login knack.js

--- login knack.js
+++ login knack.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var user = Knack.getUserAttributes();
           if (user.status == "inactive"){
-            window.location.replace("https://res-spectacular-project-95211e.webflow.io/#login/");
+            window.location.replace("https://res-spectacular-project-95211e.webflow.io/");
   }
   });
